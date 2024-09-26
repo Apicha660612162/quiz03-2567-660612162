00976ba289231c12e349b680fbb69962c60b49c6
Apicha660612162 <apichalerdjintanakid@gmail.com>
2024-09-26 15:11:19 +0700

    GG

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
   return NextResponse.json({
     ok: true,
-    fullName: "Dome Wick",
-    studentId: "660610999",
+    fullName: "Apicha Lerdjintanakit",
+    studentId: "660612162",
   });
 };
--- src/app/api/user/login/route.ts
+++ src/app/api/user/login/route.ts
@@ -2,19 +2,32 @@ import jwt from "jsonwebtoken";
 
 import { DB, readDB } from "@lib/DB";
 import { NextRequest, NextResponse } from "next/server";
+import { Database } from "@lib/types";
 
 export const POST = async (request: NextRequest) => {
   readDB();
+  const body = await request.json();
+  const { username, password } = body;
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Username or Password is incorrect",
-  //   },
-  //   { status: 400 }
-  // );
+  
+  readDB();
+  const user = (<Database>DB).users.find(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (!user) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Username or password is incorrect",
+      },
+      { status: 400 }
+    );
+  }
+  const secret = process.env.JWT_SECRET || "This is another secret"
 
   const token = "Replace this with token creation";
 
-  return NextResponse.json({ ok: true, token });
+  
+  return NextResponse.json({ ok: true, token,username });
 };
